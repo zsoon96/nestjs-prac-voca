dbbb4a29b21db0f9bfa91849ea71d7d02a9008da
zsoon96 <pres0616@gmail.com>
2022-09-13 06:36:59 +0900

    게시글 전체 조회/등록/단일 조회 API

--- src/board/board.repository.ts
+++ src/board/board.repository.ts
@@ -1,6 +1,20 @@
 import {EntityRepository, Repository} from "typeorm";
 import {Board} from "./board.entity";
 import {CustomRepository} from "./typeorm-custom.decorator";
+import {CreateBoardDto} from "./dto/create-board.dto";
 
 @CustomRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+export class BoardRepository extends Repository<Board> {
+
+    async createBoard(createBoardDto : CreateBoardDto) : Promise<Board> {
+        const { title, content } = createBoardDto
+
+        const board = this.create({
+            title,
+            content
+        })
+
+        await this.save(board)
+        return board
+    }
+}
---
+++ src/board/dto/create-board.dto.ts
@@ -0,0 +1,4 @@
+export class CreateBoardDto {
+    title: string;
+    content: string;
+}
