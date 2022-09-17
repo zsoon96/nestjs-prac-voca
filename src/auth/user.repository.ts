19e48cb269a1d8f70c9e14a2b494e6c0673cadd9
zsoon96 <pres0616@gmail.com>
2022-09-17 21:44:54 +0900

    회원가입 시, 비밀번호 암호화하여 저장(bcrypt)

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,12 +3,17 @@ import {User} from "./user.entity";
 import {CustomRepository} from "../board/typeorm-custom.decorator";
 import {UserCreateDto} from "./dto/create-user.dto";
 import {ConflictException, InternalServerErrorException} from "@nestjs/common";
+import * as bcrypt from 'bcryptjs'
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser ( userCreateDto : UserCreateDto ) : Promise<void> {
         const { email, password, username, nickname, birth, phone } = userCreateDto
-        const user = this.create ({ email, password, username, nickname,birth, phone })
+
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const user = this.create ({ email, password: hashedPassword, username, nickname, birth, phone })
 
         try {
             await this.save(user)
