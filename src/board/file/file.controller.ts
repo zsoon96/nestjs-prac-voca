924b8909c8dfea1c40cb55305852fc605ff7e836
zsoon96 <pres0616@gmail.com>
2022-09-29 06:58:45 +0900

    S3 단일 파일 삭제 테스트 (deleteObject)

--- src/board/file/file.controller.ts
+++ src/board/file/file.controller.ts
@@ -1,17 +1,11 @@
-import {
-    Controller,
-    Post,
-    UploadedFile,
-    UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { FileService } from './file.service';
+import {Controller, Post, UploadedFile, UseInterceptors,} from '@nestjs/common';
+import {FileInterceptor} from '@nestjs/platform-express';
+import {FileService} from './file.service';
 
 
 @Controller('file')
 export class FileController {
     constructor(private readonly fileService: FileService) {}
-
 
     @Post('upload')
     @UseInterceptors(FileInterceptor('file'))
--- src/board/file/file.service.ts
+++ src/board/file/file.service.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
+import * as AWS from 'aws-sdk'
+
+const s3 = new AWS.S3({
+    accessKeyId: 'AWS_SECRET_ACCESS_KEY',
+    secretAccessKey: 'AWS_SECRET_ACCESS_KEY',
+    region: 'AWS_BUCKET_REGION',
+});
 
 @Injectable()
 export class FileService {
@@ -6,7 +13,33 @@ export class FileService {
         if (!file) {
             throw new BadRequestException('파일이 존재하지 않습니다.');
         }
+        const deleteParams = {
+            Bucket: 'AWS_BUCKET_NAME',
+            Key: '57bc99e431916ec4bc062399e48d585d_1664285480629.jpeg',
+        };
+
+        // return await s3.send(new DeleteBucketCommand(deleteParams));
+
+        // try {
+        //     return await s3.deleteObject(deleteParams).promise();
+        // } catch (e) {
+        //     console.log(e);
+        // }
+
+        try {
+            s3.deleteObject(deleteParams, function (error, data) {
+                if (error) {
+                    console.log('err: ', error, error.stack);
+                } else {
+                    console.log(data, " 정상 삭제 되었습니다.");
+                }
+            })
+        } catch(err) {
+            console.log(err);
+            throw err;
+        }
 
         return { file };
     }
+
 }
