2d5456a32bd122dff86852e0958018b2cecc6ad2
zsoon96 <pres0616@gmail.com>
2022-10-04 07:05:18 +0900

    로깅 기능 적용 (LoggerService)

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -2,7 +2,7 @@ import {
     Body,
     Controller,
     Delete,
-    Get, Inject,
+    Get, Inject, InternalServerErrorException, LoggerService,
     Param,
     Patch,
     Post,
@@ -16,14 +16,15 @@ import {FileService} from './file.service';
 import {CreateFileUploadDto} from "./dto/create-file.dto";
 import {UpdateFileUploadDto} from "./dto/update-file.dto";
 import {HttpExceptionFilter} from "../common/exception-filter";
-import {WINSTON_MODULE_PROVIDER} from "nest-winston";
+import {WINSTON_MODULE_PROVIDER, WINSTON_MODULE_NEST_PROVIDER} from "nest-winston";
 import { Logger as WinstonLogger } from "winston"
 
 @UseFilters(HttpExceptionFilter)
 @Controller('file')
 export class FileController {
     constructor(private readonly fileService: FileService,
-                @Inject(WINSTON_MODULE_PROVIDER) private readonly logger : WinstonLogger) {
+                @Inject(WINSTON_MODULE_PROVIDER) private readonly logger : WinstonLogger,
+                @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger2: LoggerService) {
     }
 
     // 파일 업로드
@@ -63,7 +64,20 @@ export class FileController {
     // 파일 삭제 (단일)
     @Delete(':id')
     deleteFile(@Param('id') fileId: number) {
+        this.printLoggerSeviceLog(fileId);
         return this.fileService.deleteFile(fileId)
+    }
+
+    private printLoggerSeviceLog(fileId: number) {
+        try {
+            throw new InternalServerErrorException('test');
+        } catch (e) {
+            this.logger2.error('error: ' + JSON.stringify(fileId), e.stack);
+            this.logger2.warn('warn: ' + JSON.stringify(fileId));
+            this.logger2.log('log: ' + JSON.stringify(fileId));
+            this.logger2.verbose('verbose: ' + JSON.stringify(fileId));
+            this.logger2.debug('debug: ' + JSON.stringify(fileId));
+        }
     }
 
     @Get(':id')
